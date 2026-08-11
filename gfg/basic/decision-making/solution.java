07bcf56f1bdccfdc2a0b8f1dd8677aa20379447e
Nitin <coder83758@gmail.com>
2026-08-11 13:55:49 +0530

    ✅ Decision Making | Basic | Java | Runtime: N/A | Memory: N/A | 2026-08-11

---
+++ gfg/basic/decision-making/solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        // code here
+        if(n<m){
+            System.out.print("less");
+        }else if(n>m){
+            System.out.print("greater");
+        }else{
+            System.out.print("equal");
+        }
+    }
+}
