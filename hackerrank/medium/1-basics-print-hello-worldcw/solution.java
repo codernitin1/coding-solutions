eac8438f4e6f947c207e21fcacc232dfd9fb6fae
Nitin <coder83758@gmail.com>
2026-09-08 20:34:30 +0530

    ✅ 1. Basics - Print Hello World(CW) | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-08

---
+++ hackerrank/medium/1-basics-print-hello-worldcw/solution.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+          System.out.println("Hello World. I am here.");
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
