50ea8f434e064dadc71f68cdca9dd99a2df4ad64
Nitin <coder83758@gmail.com>
2026-09-08 21:34:07 +0530

    ✅ 3. Basic - Quotient-Reminder Calculator(CW) | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-08

---
+++ hackerrank/medium/3-basic-quotient-reminder-calculatorcw/solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+          int a=1042;
+        int b =7;
+        int quo =a/b;
+        int rem =a%b;
+        System.out.print(quo +" "+ rem);
+    }
+}
