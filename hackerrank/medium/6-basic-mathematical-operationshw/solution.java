2c889f0755b2c784ca50fdc14b94885ffcdc64ba
Nitin <coder83758@gmail.com>
2026-09-08 21:42:39 +0530

    ✅ 6. Basic - Mathematical Operations(HW) | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-08

---
+++ hackerrank/medium/6-basic-mathematical-operationshw/solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner nitin=new Scanner(System.in);
+        int ram=nitin.nextInt();
+        int shyam=nitin.nextInt();
+        System.out.println(ram+shyam);
+        System.out.println(ram-shyam);
+        System.out.println(ram*shyam);
+        System.out.println(ram/shyam);
+        System.out.println(ram%shyam);
+    }
+}
