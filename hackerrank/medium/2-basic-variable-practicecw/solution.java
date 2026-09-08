5b3fe240f7d6d59b424807aa2d36e16606c676e7
Nitin <coder83758@gmail.com>
2026-09-08 20:36:34 +0530

    ✅ 2. Basic Variable Practice(CW) | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-08

---
+++ hackerrank/medium/2-basic-variable-practicecw/solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+         int var = 10;
+        System.out.println("My num is "+var);
+       
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
