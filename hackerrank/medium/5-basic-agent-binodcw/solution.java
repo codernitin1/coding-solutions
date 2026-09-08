1d9ddf692f728113db50595f2e78c518e87b8b5d
Nitin <coder83758@gmail.com>
2026-09-08 21:41:04 +0530

    ✅ 5. Basic - Agent Binod(CW) | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-08

---
+++ hackerrank/medium/5-basic-agent-binodcw/solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner nitin=new Scanner(System.in);
+        String ram=nitin.nextLine();
+        System.out.println("Hi my name is Agent "+ram);
+    }
+}
