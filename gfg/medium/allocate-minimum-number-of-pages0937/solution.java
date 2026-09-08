2acbf7bda7d3650c68c666cd1801116ef24337c3
Nitin <coder83758@gmail.com>
2026-09-08 09:32:13 +0530

    ✅ Allocate Minimum Pages | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-08

---
+++ gfg/medium/allocate-minimum-number-of-pages0937/solution.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int findPages(int[] arr, int k) {
+        // code here
+        int m = arr.length-1;
+        int sum = 0;
+        
+       if(k > arr.length){
+           return -1;
+       }
+       long low = 0;
+       long high = 0;
+       
+       for(int page:arr){
+           low = Math.max(low,page);
+           high += page;
+       }
+       long ans = 0;
+       
+       while(low <= high){
+           long mid  =  low + (high - low)/2;
+           if(ispossible(arr, k, mid)){
+               ans = mid;
+               high = mid - 1;
+           }else {
+               low = mid +1;
+           }
+       }
+           return (int)ans;
+    }private boolean ispossible(int []arr , int k, long maxpages ){
+        int student = 1;
+        long curpage =0;
+        
+        for(int pages:arr){
+            if(curpage + pages <= maxpages){
+                curpage +=pages;
+            }else{
+                student++;
+                curpage = pages;
+                
+                if(student > k) return false;
+            }
+        }
+        return true;
+    }
+}
