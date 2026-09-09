dd8b1140b0e3c6c8c9efc35fcf3d4bbaa0d856b1
Nitin <coder83758@gmail.com>
2026-09-09 22:21:50 +0530

    ✅ Array Traversal | Basic | Java | Runtime: N/A | Memory: N/A | 2026-09-09

--- gfg/basic/array-traversal/solution.java
+++ gfg/basic/array-traversal/solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public static void arrayTraversal(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i <= arr.length-1; i++) {
             System.out.print(arr[i] + " ");
         }
     }
