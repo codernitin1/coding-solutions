ce619edf4c424f071783adcaf2579e134aa08a19
Nitin <coder83758@gmail.com>
2026-09-10 08:57:35 +0530

    ✅ Prime Number | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-10

--- gfg/easy/prime-number2314/solution.java
+++ gfg/easy/prime-number2314/solution.java
@@ -1,15 +1,14 @@
 class Solution {
     static boolean isPrime(int n) {
         // code here
-        int temp =0;
-        if(n==1) return false;
-        
-        for(int i=2;i<=n/2;i++){
-            if(n%i == 0){
-                temp++;
+       if (n <= 1) return false;
+        if (n <= 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) {
+                return false;
             }
         }
-        if(temp>0)return false;
-        else return true;
+        return true;
     }
 }
