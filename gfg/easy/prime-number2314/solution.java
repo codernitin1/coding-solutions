0b5f68f9b58bc9155fb65aa8e1fe5c4b48354f00
Nitin <coder83758@gmail.com>
2026-09-10 09:02:56 +0530

    ✅ Prime Number | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-10

--- gfg/easy/prime-number2314/solution.java
+++ gfg/easy/prime-number2314/solution.java
@@ -4,7 +4,7 @@ class Solution {
        if (n <= 1) return false;
         if (n <= 3) return true;
         if (n % 2 == 0 || n % 3 == 0) return false;
-        for (int i = 5; i * i <= n; i++) {
+        for (int i = 5; i <= n/2; i++) {
             if (n % i == 0 || n % (i + 2) == 0) {
                 return false;
             }
