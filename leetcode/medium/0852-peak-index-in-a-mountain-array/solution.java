fd92e8fc3c62e226f2639100d863f2d2cd7022f4
Nitin <coder83758@gmail.com>
2026-09-15 21:16:12 +0530

    ✅ Peak Index in a Mountain Array | Medium | Java | Runtime: 0 ms (100.00%) | Memory: 80.2 MB | 2026-09-15

--- leetcode/medium/0852-peak-index-in-a-mountain-array/solution.java
+++ leetcode/medium/0852-peak-index-in-a-mountain-array/solution.java
@@ -1,12 +1,16 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int Fmax = arr.length;
-        int max = arr[0];
-        for(int i=0;i<Fmax;i++){
-            if(arr[i]<=max){
-                return i;
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] < arr[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
             }
         }
-        return 1;
+
+        return left;
     }
 }
