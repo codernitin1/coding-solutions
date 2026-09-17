b3a422c6cf62bb932e73d4627deff451285862f8
Nitin <coder83758@gmail.com>
2026-09-17 08:05:39 +0530

    ✅ SHARING | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-17

--- codechef/medium/SHARING/solution.java
+++ codechef/medium/SHARING/solution.java
@@ -7,6 +7,16 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		
+		if(a+b %2 !=0){
+		    System.out.println("-1");
+		}else {
+		    int cookies = a-b/2;
+		    System.out.println(cookies);
+		}
 	}
 }
