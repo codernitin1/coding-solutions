692ef26c9f9b56efbb36600a48701dac289c1f9a
Nitin <coder83758@gmail.com>
2026-09-17 09:49:15 +0530

    ✅ Koko Eating Bananas | Medium | Java | Runtime: 7 ms (83.68%) | Memory: 48.1 MB | 2026-09-17

--- leetcode/medium/0875-koko-eating-bananas/solution.java
+++ leetcode/medium/0875-koko-eating-bananas/solution.java
@@ -8,16 +8,14 @@ class Solution {
                 max=piles[i];
             }
         }
-
+        
         int e=max;
         while(s<e){
             int mid=(s+e)/2;
-
             int time=0;
             for(int i=0;i<piles.length;i++){
                 time+= (piles[i]+mid-1)/mid;
             }
-
             if(time>h){
                 s=mid+1;
             }else{
